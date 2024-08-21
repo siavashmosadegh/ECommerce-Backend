413c573b692c1fcd7160fc6ddaa5158361eca17e
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-08-21 19:15:13 +0330

    development vs production ERRORs

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -3,8 +3,18 @@ export default (err, req, res, next) => {
         statusCode: err?.statusCode || 500,
         message: err?.message || "Internal Server Error"
     };
-    
-    res.status(error.statusCode).json({
-        message: error.message
-    });
+
+    if (process.env.NODE_ENV === "development") {
+        res.status(error.statusCode).json({
+            message: error.message,
+            error: err,
+            stack: err?.stack
+        });
+    }
+
+    if (process.env.NODE_ENV === "production") {
+        res.status(error.statusCode).json({
+            message: error.message
+        });
+    }
 };
