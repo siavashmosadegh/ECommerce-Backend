5adcf9ec4a35d0f2845945ad9da91c323643d990
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-09-10 20:52:26 +0330

    a problem was fixed: when token == null or token == undefined application crashed , now i works properly ...

--- middlewares/authenticateTokenV1.js
+++ middlewares/authenticateTokenV1.js
@@ -5,13 +5,17 @@ async function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null)  {
-        res.status(401);
-    }
+    // if (typeof req.headers['authorization'] === 'undefined')  {
+    //     console.log("hey")
+    //     res.status(401);
+    // }
 
     verify(token, process.env.JWT_SECRET, (err,user) => {
         if (err) {
-            res.status(403);
+            console.log(err);
+            res.status(403).json({
+                error: err
+            });
         }
         
         req.user = user;
