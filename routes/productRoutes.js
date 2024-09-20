2daea4eb640236888fdb1037026d10c9787ca8a7
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-09-20 13:30:03 +0330

    role admin was also added to updateProduct & deleteProduct

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -16,7 +16,7 @@ const router = express.Router();
 router.route("/products").get(getProducts);
 router.route("/admin/products").post( authenticateToken, authorizeRoles("admin") ,createProducts);
 router.route("/products/:id").get(getProductDetails);
-router.route("/admin/products/:id").put( authenticateToken, updateProduct);
-router.route("/admin/products/:id").delete(authenticateToken, deleteProduct);
+router.route("/admin/products/:id").put( authenticateToken, authorizeRoles("admin") , updateProduct);
+router.route("/admin/products/:id").delete(authenticateToken, authorizeRoles("admin") , deleteProduct);
 
 export default router;
