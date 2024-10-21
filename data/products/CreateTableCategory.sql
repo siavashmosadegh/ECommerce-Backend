1ecf6d760495e0eefa53619223f72cf84902b9ab
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-10-21 21:36:38 +0330

    added FarsiCategoryName column to Category

--- data/products/CreateTableCategory.sql
+++ data/products/CreateTableCategory.sql
@@ -3,6 +3,7 @@ use mydatabase;
 CREATE TABLE Category (
     CategoryID INT IDENTITY(1,1) PRIMARY KEY,
     CategoryName NVARCHAR(100) NOT NULL,
+    FarsiCategoryName NVARCHAR(100) NOT NULL,
     Description NVARCHAR(255) NULL,
     CreatedDate DATETIME DEFAULT GETDATE()
 );
