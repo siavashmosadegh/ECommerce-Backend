b999f2c26ff13cee38f882457e97e889fb5ddfc4
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-11-10 16:50:05 +0330

    CarCreateTable

---
+++ data/SqlServerTables/CarCreateTable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Car] (
+    [CarID]             INT           IDENTITY (1, 1) NOT NULL,
+    [CarBrandID]        INT           NOT NULL,
+    [CarModel]          NVARCHAR (50) NOT NULL,
+    [CarModelFarsi]     NVARCHAR (50) NOT NULL,
+    [TrimLevel]         NVARCHAR (50) NULL,
+    [TrimLevelFarsi]    NVARCHAR (50) NULL,
+    [SubTrimLevel]      NVARCHAR (50) NULL,
+    [SubTrimLevelFarsi] NVARCHAR (50) NULL,
+    [Year]              INT           NULL,
+    PRIMARY KEY CLUSTERED ([CarID] ASC),
+    FOREIGN KEY ([CarBrandID]) REFERENCES [dbo].[CarBrands] ([CarBrandID])
+);
