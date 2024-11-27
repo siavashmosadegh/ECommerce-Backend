4d316028f6a2965cd32982d2ea0eb903cd5822f2
siavashmosadegh <s.mosadegh@rocketmail.com>
2024-11-27 19:57:24 +0330

    ProductType Insert Data in SQL SERVER

---
+++ data/initialData/ProductTypeData.sql
@@ -0,0 +1,109 @@
+﻿use mydatabase;
+
+INSERT INTO [dbo].[ProductType]
+    (
+        [categoryID],
+        [productTypeName],
+        [productTypeNameFarsi]
+    )
+VALUES 
+(
+    1,
+    ('hood'),
+    (N'کاپوت')
+),
+(
+    1,
+    ('trunk-door'),
+    (N'درب صندوق عقب')
+),
+(
+    1,
+    ('bumper'),
+    (N'سپر')
+),
+(
+    1,
+    ('car-door'),
+    (N'درب ماشین')
+),
+(
+    1,
+    ('splash-guard'),
+    (N'گلگیر')
+),
+(
+    1,
+    ('in-front-of-window'),
+    (N'جلو پنجره')
+),
+(
+    1,
+    ('side-mirror'),
+    (N'آینه بغل')
+),
+(
+    1,
+    ('car-pedal'),
+    (N'رکاب')
+),
+(
+    1,
+    ('fender-liner'),
+    (N'شلگیر')
+),
+(
+    1,
+    ('car-door-handle'),
+    (N'دستگیره در')
+),
+(
+    1,
+    ('gel-pakhshkon'),
+    (N'گل پخش کن')
+),
+(
+    1,
+    ('lola'),
+    (N'لولا')
+),
+(
+    1,
+    ('string'),
+    (N'زه ماشین')
+),
+(
+    1,
+    ('glass'),
+    (N'شیشه ماشین')
+),
+(
+    1,
+    ('lock'),
+    (N'قفل ماشین')
+),
+(
+    1,
+    ('window-sheild-wiper'),
+    (N'برف پاک کن')
+),
+(
+    1,
+    ('shisheshoor'),
+    (N'شیشه شور')
+),
+(
+    1,
+    ('car-door-tape'),
+    (N'نوار دور درب ماشین')
+),
+(
+    1,
+    ('projector'),
+    (N'پروژکتور')
+),
+(
+    1,
+    ('daylight'),
+    (N'دیلایت')
+)
