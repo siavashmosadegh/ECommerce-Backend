54820ce076c81e2097bc3644d478101cb894ea92
siavashmosadegh <s.mosadegh@rocketmail.com>
2024-12-01 19:36:22 +0330

    ProductTypeBrand Initial Data

---
+++ data/initialData/ProductTypeBrandInitialData.sql
@@ -0,0 +1,68 @@
+﻿use mydatabase;
+
+INSERT INTO [dbo].[ProductTypeBrand]
+    (
+        [ProductTypeBrandName],
+        [ProductTypeBrandNameFarsi]
+    )
+VALUES 
+(
+    ('hannah-part'),
+    (N'هانا پارت')
+),
+(
+	('isaco'),
+	(N'ایساکو')
+),
+(
+	('saipa-yadak'),
+	(N'سایپا یدک')
+),
+(
+	('m-co'),
+	(N'امکو')
+),
+(
+	('chekad'),
+	(N'چکاد')
+),
+(
+	('mega-motor'),
+	(N'مگا موتور')
+),
+(
+	('ezam'),
+	(N'عظام')
+),
+(
+	('dina-part'),
+	(N'دینا پارت')
+),
+(
+	('arian-int'),
+	(N'آرین خودرو خاورمیانه')
+),
+(
+	('irca'),
+	(N'ایرکا')
+),
+(
+	('amirnia'),
+	(N'امیرنیا')
+),
+(
+	('iman-tak'),
+	(N'ایمن تک پیشرو')
+),
+(
+	('lanter'),
+	(N'لنتر')
+),
+(
+	('avatoseh'),
+	(N'آوا توسعه')
+),
+(
+	('balin-tech'),
+	(N'بالین تک')
+)
