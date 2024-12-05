1d4b88c485a9f68ffc0a0f2377e325ff82db4bdb
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-12-05 19:23:40 +0330

    ProductFeatures Table Updated (ProductID should not be NULL)

--- data/SqlServerTables/ProductFeaturesCreateTable.sql
+++ data/SqlServerTables/ProductFeaturesCreateTable.sql
@@ -1,8 +1,11 @@
 use mydatabase;
 
 CREATE TABLE [dbo].[ProductFeatures] (
-	ProductFeaturesID UNIQUEIDENTIFIER Primary Key DEFAULT (newid()) NOT NULL,
-	ProductID Int REFERENCES Product(ProductID),
-	Title Nvarchar(255) Not Null,
-	Explanation Nvarchar(255) Not null
+    [ProductFeaturesID] UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [ProductID]         INT              NOT NULL,
+    [Title]             NVARCHAR (255)   NOT NULL,
+    [Explanation]       NVARCHAR (255)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([ProductFeaturesID] ASC),
+    FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Product] ([ProductID])
 );
+
