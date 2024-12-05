654331bedfaa6a5d5a1dcfc79d8368a9acb04583
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-12-05 18:09:56 +0330

    updated ProductTypeBrand create table script both local and remote ...

--- data/SqlServerTables/ProductTypeBrandCreateTable.sql
+++ data/SqlServerTables/ProductTypeBrandCreateTable.sql
@@ -1,8 +1,9 @@
 use mydatabase;
 
 CREATE TABLE [dbo].[ProductTypeBrand] (
-	[ProductTypeBrandID]        INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	[ProductTypeID]             INT REFERENCES ProductType(productTypeID) NOT NULL,
-	[ProductTypeBrandName]      NVARCHAR(50) NOT NULL,
-	[ProductTypeBrandNameFarsi] NVARCHAR(50) NOT NULL
-)
+    [ProductTypeBrandID]        INT           IDENTITY (1, 1) NOT NULL,
+    [ProductTypeBrandName]      NVARCHAR (50) NOT NULL,
+    [ProductTypeBrandNameFarsi] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ProductTypeBrandID] ASC)
+);
+
