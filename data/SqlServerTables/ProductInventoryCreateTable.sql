3361b842e384d0ae4377c1f967043f179423e35f
siavashmosadegh <s.mosadegh@rocketmail.com>
2024-12-14 18:36:39 +0330

    updated ProductInventory Table slightly ...

--- data/SqlServerTables/ProductInventoryCreateTable.sql
+++ data/SqlServerTables/ProductInventoryCreateTable.sql
@@ -1,11 +1,8 @@
 CREATE TABLE [dbo].[ProductInventory] (
     [ProductInventoryID] INT      NOT NULL,
     [quantity]           INT      NOT NULL,
-    [dateCreatedAt]      DATE     NULL,
-    [timeCreatedAt]      TIME (7) NULL,
-    [dateModifiedAt]     DATE     NULL,
-    [timeModifiedAt]     TIME (7) NULL,
-    [dateDeletedAt]      DATE     NULL,
-    [timeDeletedAt]      TIME (7) NULL,
+    [CreatedAt]          DATETIME NULL,
+    [ModifiedAt]         DATETIME NULL,
+    [DeletedAt]          DATETIME NULL,
     PRIMARY KEY CLUSTERED ([ProductInventoryID] ASC)
 );
