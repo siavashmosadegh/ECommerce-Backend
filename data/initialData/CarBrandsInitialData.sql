fb5096be5050d7114089048f5f54fadcd1e2179e
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-02-09 17:35:21 +0330

    CarBrandsInitialData.sql

---
+++ data/initialData/CarBrandsInitialData.sql
@@ -0,0 +1,11 @@
+USE [mydatabase]
+GO
+SET IDENTITY_INSERT [dbo].[CarBrands] ON 
+
+INSERT [dbo].[CarBrands] ([CarBrandID], [CarBrandName], [CarBrandNameFarsi]) VALUES (1, N'saipa', N'سایپا')
+INSERT [dbo].[CarBrands] ([CarBrandID], [CarBrandName], [CarBrandNameFarsi]) VALUES (2, N'iran-khodro', N'ایران خودرو')
+INSERT [dbo].[CarBrands] ([CarBrandID], [CarBrandName], [CarBrandNameFarsi]) VALUES (3, N'mazda', N'مزدا')
+INSERT [dbo].[CarBrands] ([CarBrandID], [CarBrandName], [CarBrandNameFarsi]) VALUES (4, N'peugeot', N'پژو')
+INSERT [dbo].[CarBrands] ([CarBrandID], [CarBrandName], [CarBrandNameFarsi]) VALUES (5, N'renault', N'رنو')
+SET IDENTITY_INSERT [dbo].[CarBrands] OFF
+GO
