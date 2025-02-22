90b20de62676af49329c525ef60d0b138b768a4e
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-02-22 19:47:22 +0330

    ProductFeatures table improved and moved to host ...

--- data/SqlServerTables/ProductFeaturesCreateTable.sql
+++ data/SqlServerTables/ProductFeaturesCreateTable.sql
@@ -1,11 +1,9 @@
 use mydatabase;
 
 CREATE TABLE [dbo].[ProductFeatures] (
-    [ProductFeaturesID] UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
-    [ProductID]         INT              NOT NULL,
+    [ProductFeaturesID] UNIQUEIDENTIFIER Primary Key DEFAULT (newid()) NOT NULL,
+    [ProductID]         UNIQUEIDENTIFIER NOT NULL,
     [Title]             NVARCHAR (255)   NOT NULL,
     [Explanation]       NVARCHAR (255)   NOT NULL,
-    PRIMARY KEY CLUSTERED ([ProductFeaturesID] ASC),
     FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Product] ([ProductID])
 );
-
