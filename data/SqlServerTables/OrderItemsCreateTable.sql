58a412672161f260df97af79a26f74b06a1b54f4
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-02-22 19:54:33 +0330

    improved OrderItems and moved to host ...

--- data/SqlServerTables/OrderItemsCreateTable.sql
+++ data/SqlServerTables/OrderItemsCreateTable.sql
@@ -1,9 +1,10 @@
+use mydatabase
+
 CREATE TABLE [dbo].[OrderItems] (
     [OrderItemId] UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
-    [OrderId]     UNIQUEIDENTIFIER NULL,
-    [ProductId]   INT              NULL,
-    [Quantity]    INT              NULL,
+    [OrderId]     UNIQUEIDENTIFIER NOT NULL,
+    [ProductId]   UNIQUEIDENTIFIER NOT NULL,
+    [Quantity]    INT              NOT NULL,
     [Price]       DECIMAL (10, 2)  NULL,
-    PRIMARY KEY CLUSTERED ([OrderItemId] ASC),
     FOREIGN KEY ([OrderId]) REFERENCES [dbo].[Orders] ([OrderId])
 );
