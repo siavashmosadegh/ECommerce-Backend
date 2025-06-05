533ce85a5e484bdf50b9d1be193282885afd2cf7
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2025-06-05 16:46:56 +0330

    Cart Create Table

---
+++ data/SqlServerTables/CartCreateTable.sql
@@ -0,0 +1,10 @@
+use mydatabase;
+
+CREATE TABLE Cart (
+    CartId UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
+    UserID INT NULL, -- NULL means guest user
+    CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME,
+    IsActive BIT DEFAULT 1,
+    FOREIGN KEY (UserID) REFERENCES Users (UserID)
+);
